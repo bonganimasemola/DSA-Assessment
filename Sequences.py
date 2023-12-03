95532b0dd2c37818f629903acf901192f08fa332
bonganimasemola <bonganimasemola@icloud.com>
2023-12-03 10:10:08 +0300

    set up up function to remove duplicates for a given sequence

--- Sequences.py
+++ Sequences.py
@@ -1,5 +1,19 @@
-def remove_duplicates():
+def remove_duplicates(sequence):
     """
-    Remove duplicates from a list
+    create a new list and iterating through the elements of the input sequence while adding each unique element to the new list
     """
-    pass
+    
+    seen = ()
+    result = []
+    
+    for item in sequence:
+        if item not in seen:
+            # seen.add(item)
+            result.append(item)
+            seen += (item,)
+    return result 
+
+
+input_sequence = [2, 3, 2, 4, 5, 3, 6, 7, 5]
+result = remove_duplicates(input_sequence)
+print(result) 
